6090583a96a514b506636b7be7e0db3072b5db60
felipermendess <felipermendes04@gmail.com>
2025-03-18 22:19:06 -0400

    introdução as rotas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+// verbo http - get, post, put, patch, delete
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('admin/usuarios', function () {
+    // return 'Hello World';
+
+    // json - laravel identifica e transforma array em json
+    return [
+        'id'=>1,
+        'name'=>'John'
+    ];
 });
