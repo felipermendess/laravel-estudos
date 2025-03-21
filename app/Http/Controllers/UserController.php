967089065b036606f77958a58d045d6fa78e9df6
felipermendess <felipermendes04@gmail.com>
2025-03-20 20:28:14 -0400

    blade template views

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,14 +8,30 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function getUsers () {
+        // buscando todos os usuários do banco
+        $users = User::all();
+
+        // retornando view para a função que é executada na rota e setando variáveis para o blade template
+        return view('users.index', [
+            'greeting' => 'Hello Peoples',
+            'users' => $users
+            // 'users' => [
+            //     'id' => 1,
+            //     'name' => 'Reece'
+            // ]
+        ]);
+        
         // return 'Hello Users';
-        return [
-            'id'=>1,
-            'name'=>'Smith'
-        ];
+        // return [
+        //     'id'=>1,
+        //     'name'=>'Smith'
+        // ];
     }
 
     public function show (User $user) {
-        return $user;
+        return view('users.show', [
+            'user' => $user
+        ]);
+        // return $user;
     }
 }
