0ce9ad156cd60d9a3b4983d98b405b717bab395c
felipermendess <felipermendes04@gmail.com>
2025-03-25 22:24:14 -0400

    criando model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+    // atributo protegido, array de campos de uma tabela liberados para salvamento em massa
+    protected $fillable = [
+        'title', 'body'
+    ];
+
+    // config gerais do model
+    // relações
+    // 1->1
+    // 1->N
+    // N->N
 }
