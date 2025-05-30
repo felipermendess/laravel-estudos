8f1b3e318b0a663d6aa4813f8b3491ce29bef200
felipermendess <felipermendes04@gmail.com>
2025-05-29 21:38:37 -0400

    herança de template com extends, section e yield

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,13 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Aula de Laravel')</title>
-    @vite('resources/css/app.css')
+    <title>@yield('title', 'Laravel app')</title>
 </head>
 
 <body>
-    @yield('content')
-    @vite('resources/js/app.js')
+    @section('sidebar')
+        Conteúdo padrão
+    @show
+    <br>
+    @yield('content', 'Conteúdo padrão do yield')
 </body>
 
 </html>
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,112 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome User!</title>
-</head>
-<body>
-    @include('heading', ['title' => 'Usuários'])
+@extends('layouts.default')
 
-    <!-- foreach ($users as $user)
-        include('user.show_users', ['user' => $user])
-    endforeach -->
+@section('title', 'Perfil')
 
+@section('sidebar')
+    Meu conteúdo personalizado <br><br>
+    @parent <br>
     @each('user.show_users', $users, 'user')
+@endsection
 
-    @if (count($users) === 1)
-        Tenho um usuário
-    @elseif (count($users) > 1)
-        Tenho mais de um usuário
-    @else
-        Não sei se tenho usuários
-    @endif
+@section('content')
+	Contéudo do Yield
+@endsection
 
-    <br><br>
-
-    @unless(!count($users))
-        Tenho usuários
-    @endunless
-
-    <br><br>
-
-    @isset($users)
-        A variável existe
-    @endisset
-
-    <br><br>
-
-    @empty($users)
-        A variável está vazia
-    @endempty
-
-    @for($i = 0; $i <= 10; $i++)
-        {{ $i }}
-    @endfor
-
-    <br><br>
-
-    @foreach ($users as $user)
-        {{ $user->email }} <br>
-    @endforeach
-
-    <br>
-
-    @forelse ($users as $user)
-        {{ $user->name }} <br>
-    @empty
-        Vazio
-    @endforelse
-
-    <br>
-
-    @php
-        $i = 0;
-    @endphp
-    @while($i <= 10)
-        {{ $i++ }}
-    @endwhile
-
-    <br><br>
-
-    @foreach ($users as $user)
-        @if ($user->id === 1)
-            @continue
-        @endif
-
-        {{ $user->remember_token }} <br>
-
-        @if ($user->id === 5)
-            @break
-        @endif
-    @endforeach
-
-    <br><br>
-
-    @switch (count($users))
-        @case(0)
-            Não tenho usuários
-            @break
-        @case(12)
-            Tenho 12 usuários
-            @break
-        @default
-            Não sei quantos usuários tenho
-    @endswitch
-
-    <br><br>
-
-    @foreach ($users as $user)
-        {{ dd($loop) }}
-    @endforeach
-
-    {{ dd($users) }}
-
-    <!-- includes
-        include -- Inclui um arquivo Blade
-        includeIf -- Inclui um arquivo Blade se ele existir
-        includeWhen -- Inclui um arquivo Blade quando uma condição for verdadeira
-        includeUnless -- Inclui um arquivo Blade quando uma condição for falsa
-        includeFirst -- Inclui o primeiro arquivo Blade que existir em uma lista de arquivos -->
-</body>
-</html>
+<!--
+    Diferença entre section e yield:
+    - section: abre blocode código, permite adicionar conteúdo personalizado e chamar o padrão, blocos de código (sidebar, footer, etc.)
+    - yield: não abre bloco de código, não aparece conteúdo padrão, conteúdos principais (texto da página, etc.)
+-->
